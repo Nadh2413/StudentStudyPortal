16a8f83fafd9db2d41a6b38fd9cc06acc752a97f
cdhuan <cdhuan1003@gmail.com>
2023-03-27 22:29:11 +0700

    Them note

---
+++ dashBoard/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from . models import *
+
+# class NotesForm(forms.ModelForm):
+#     class Meta:
+#         models = Notes
+#         fields = ['titles','description']
+
+    
